c44badeec5499706abf0a1c74cf48fb4d31b33c7
jgoudiab <jeangabrielgoudiaby@gmail.com>
2025-02-26 14:50:11 +0000

    project init

---
+++ osint/main.py
@@ -0,0 +1,32 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Welcome to passive v1.0.0')
+    parser.add_argument('-fn', type=str, help='Search with full-name')
+    parser.add_argument('-ip', type=str, help='Search with ip address')
+    parser.add_argument('-u', type=str, help='Search with username')
+
+    args = parser.parse_args()
+
+    if not any([args.fn, args.ip, args.u]):
+        print("Veuillez spécifier une option valide (-fn, -ip, -u)")
+        return
+
+    if args.fn:
+        #result = appel de la fonction search full name
+        # enregistrer les resultats dans un fichier
+        print("search with full name")
+    elif args.ip:
+        #result = appel de la fonction Search with ip address
+        # enregistrer les resultats dans un fichier
+        print("Search with ip address")
+    else:
+        #result = appel de la fonction Search with username
+        # enregistrer les resultats dans un fichier
+        print("Search with username")
+    
+
+
+if __name__ == "__main__":
+    main()
+
